e2d92e62ab67204e80fca18ec007e4b9bae28810
fang01.liu <fang01.liu@meditrusthealth.com>
2024-12-04 11:23:47 +0000

    add maxProductSubarray.ts

---
+++ BinaryTree/BST-KthSmallestElement.ts
@@ -0,0 +1,42 @@
+class TreeNode{
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode|null){
+        this.val = (val == undefined ? 0: val)
+        this.left = (left == undefined ? null : left)
+        this.right = (right == undefined ? null: right)
+    }
+}
+
+function  kthSmallest(root: TreeNode|null, k: number): number|undefined{
+    let arr : number[] = []
+    inOrder(root)
+    function inOrder(root: TreeNode| null){
+        if(!root){
+            return null
+        }
+        inOrder(root.left)
+        arr.push(root.val)
+        inOrder(root.right)
+    }
+    for(let i = 0; i < arr.length; i++){
+        if(i == k-1){
+            return arr[i]
+        }
+    }
+}
+
+// Test case 1
+const root1 = new TreeNode(3)
+root1.left = new TreeNode(1, null, new TreeNode(2))
+root1.right = new TreeNode(4)
+const k1 = 1
+console.log(`Test Case 1: Expected Output: 1, Actual Output: ${kthSmallest(root1, k1)}`);
+
+// Test case 2
+const root2 = new TreeNode(5)
+root2.left = new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4))
+root2.right = new TreeNode(6)
+const k2 = 3
+console.log(`Test Case 2: Expected Output: 3, Actual Output: ${kthSmallest(root2, k2)}`);
