a04daa113b22f65013c5b865fd70c1c4a83313b6
fang01.liu <fang01.liu@meditrusthealth.com>
2024-12-28 13:50:32 +0000

    add rotate.ts

---
+++ ArrayString/rotate.ts
@@ -0,0 +1,25 @@
+// return new arr
+function rotateReturn(nums: number[], k: number): number[]{
+    let sliceNum = nums.length - (k%nums.length)
+    console.log(sliceNum)
+    console.log(nums.slice(0, sliceNum), nums.slice(sliceNum))
+    const arr = (nums.slice(sliceNum)).concat(nums.slice(0, sliceNum))
+    return arr
+}
+// const nums = [1,2,3,4,5,6,7], k = 3
+const nums = [-1,-100,3,99], k = 3
+console.log(rotate(nums, k))
+
+// Do not return anything, modify nums in-place instead. 
+function rotate(nums: number[], k: number): void{
+    let sliceNum = nums.length - (k%nums.length)
+    nums.unshift(...nums.splice(sliceNum))
+    console.log(nums)
+}
+
+// remove == splice(startIndex)
+
+let str = ['fruit', 'apple', 'pie', 'banans']
+console.log(str.splice(1))   //[ 'apple', 'pie', 'banans' ]
+console.log(str)   // [ 'fruit' ]
+
